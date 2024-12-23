82255a21a0cc5249672f95b8cdf981b55cd43e55
Xaminame <ivankuznecov25.06.06@gmail.com>
2024-12-23 19:26:40 +0300

    Добавлен файл

--- все_лаб/2 лаба/1.1.py
+++
@@ -1,5 +0,0 @@
-#Лабороторная работа №1. Задание №1
-#Группа БВТ2403 - Кузнецов Иван
-def square(number):
-  return number**2
-print(square(55))
--- все_лаб/3 лаба/3.1.py
+++
@@ -1,9 +0,0 @@
-#with open("example.txt", 'r') as file:
-#    content = file.read()
-#print(content)
-
-
-
-#with open('example.txt','r') as file:
-#    for line in file:
-#        print(line)
